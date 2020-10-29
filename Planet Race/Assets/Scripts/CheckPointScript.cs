8760941b30baf672c1fca61fd0e47089d48c427e
Lasse Berner <98yoda@gmx.de>
2020-10-29 15:57:07 +0100

    Planet Race
    Material und Szenen Richtig benannt

---
+++ Planet Race/Assets/Scripts/CheckPointScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPointScript : MonoBehaviour
+{
+    public Transform RaceManager;
+    private RaceManager _manager;
+    private string myName;
+
+    void Awake()
+    {
+        _manager = GameObject.FindObjectOfType<RaceManager>();
+        myName = this.name;
+    }
+
+    void OnTriggerEnter()
+    {
+        //Debug.Log("on trigger enter CheckPointScrpt");
+
+        _manager.triggerStatus = true;
+        _manager.nameOfActive = int.Parse(myName);
+    }
+}
---
+++ Planet Race/Assets/Scripts/RaceManager.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaceManager : MonoBehaviour
+{
+    public Transform playerCar;
+    public Transform startPoint; //leres Object mit korrektem Transform für startendes Auto
+
+    public Transform checkPointMesh; //Mesh bzw Modell vom aktiven Checkpoint
+    private Transform parentOfCheckPoints; //Parent von allen Checkpoints, heißt "AllCheckPoints"
+    private int numberOfCheckPoints;
+    private int numberOfActive; //Nummer in RaceManager
+    public int nameOfActive; //Name der Objecte der CheckPointScricpts
+    private int checkPointLayer;
+    public bool triggerStatus { get; set; }
+
+//hauptprogramm
+    void Awake()
+    {
+        setupCheckPoints();
+        checkPointLayer = LayerMask.NameToLayer("CheckPoint");
+    }
+
+    void Start()
+    {
+        startRace();
+        triggerStatus = false;
+    }
+
+    void Update()
+    {
+        //setzt Mesh auf naechste Checkpoints
+        updateCheckPoints();
+    }
+
+/*
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.layer != checkPointLayer)
+        {
+            return;
+        }
+        else
+        {
+            triggerStatus = true;
+            Debug.Log("enter Trigger RaceManager");
+        }
+    }
+*/
+
+//methoden
+    void setupCheckPoints()
+    {
+        parentOfCheckPoints = GameObject.Find("AllCheckPoints").transform;
+        numberOfCheckPoints = parentOfCheckPoints.childCount;
+        numberOfActive = 0; //startwert, collider der Ziellinie
+    }
+
+    void startRace()
+    {
+        positionMesh(numberOfActive);
+
+        //setzt Auto an StartOrt
+        playerCar.position = startPoint.position;
+        playerCar.rotation = startPoint.rotation;
+
+        //Startet counter (3, 2, 1, GO!)
+
+        //Startet anschliessend Musik
+
+        //setzt ersten CheckPoint
+    }
+
+    void updateCheckPoints()
+    {
+        if (triggerStatus)
+        {
+            //Debug.Log("nameOfActive = " + nameOfActive);
+            //Debug.Log("numberOfActive = " + numberOfActive);
+            if (nameOfActive == numberOfActive)
+            {
+                if (numberOfActive == numberOfCheckPoints-1)
+                {
+                    numberOfActive = 0;
+                    positionMesh(numberOfActive);
+                }
+                else
+                {
+                    numberOfActive++;
+                    positionMesh(numberOfActive);
+                }
+            }
+        }
+        //bei Trigger numberOfActive++; + positionMesh();
+    }
+
+    void positionMesh(int numberOfPoint)
+    {
+        //transform holen
+        Transform posOfPoint = parentOfCheckPoints.transform.GetChild (numberOfPoint); //0 bis childCount
+
+        checkPointMesh.position = posOfPoint.position;
+        checkPointMesh.rotation = posOfPoint.rotation;
+        triggerStatus = false;
+    }
+}
