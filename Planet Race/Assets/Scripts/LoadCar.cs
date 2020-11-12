dd8ad8311329abbb587d1b8c817ac0225f46014d
Lasse Berner <98yoda@gmx.de>
2020-11-12 16:41:49 +0100

    Menu
    Auswählen kann man, aber es hat keinen einfluss, un das debug log bzw die uebergabe des currentPlanet klappt iwi immwenoch nicht, ka warum...

---
+++ Planet Race/Assets/Scripts/LoadCar.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadCar : MonoBehaviour
+{
+    public GameObject[] carPrefabs;
+    public Transform spawnPoint;
+
+    void Start()
+    {
+        int selectedCar = PlayerPrefs.GetInt("selectedCar");
+        GameObject prefab = carPrefabs[selectedCar];
+        //GameObject clone = Instanciate(prefab, spawnPoint, Quaternion.identity);
+    }
+}
---
+++ Planet Race/Assets/Scripts/PlanetSelection.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetSelection : MonoBehaviour
+{
+    public GameObject[] planets; //Macht GameObject Planets zu einem Array
+    public int selectedPlanet = 0; //index das Array
+    //public int selectedPlanet { get; set; }
+    public MainMenu mainMenu;
+
+    public void NextPlanet()
+    {
+        planets[selectedPlanet].SetActive(false);
+        selectedPlanet = (selectedPlanet + 1) % planets.Length;
+        planets[selectedPlanet].SetActive(true);
+    }
+
+    public void PreviousPlanet()
+    {
+        planets[selectedPlanet].SetActive(false);
+        selectedPlanet--;
+        if (selectedPlanet < 0)
+        {
+            selectedPlanet += planets.Length;
+        }
+        planets[selectedPlanet].SetActive(true);
+    }
+
+    void StartGame() //von MainMenü rufen lassen
+    {
+        //PlayerPrefs.SetInt("selectedPlanet", selectedPlanet); //speichert Daten, zwischen szenen
+        //SceneManager.LoadScene("Mars", LoadSceneMode.Single);
+    }
+
+    void Awake()
+    {
+        mainMenu = GameObject.FindObjectOfType<MainMenu>();
+    }
+
+    void Update()
+    {
+        mainMenu.updateCurrentPlanet(selectedPlanet);
+    }
+}
